60179d88e793ce23c99b6c3eea0c54506c198b2c
Ayman Sharaf <ayman.sharaf@echotechit.com>
2019-12-14 21:22:04 +0300

    Rename Contoller

---
+++ IReckonu.DataImportingTool/DataImportController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using IReckonu.DataImportingTool.Application.Abstractions;
+using IReckonu.DataImportingTool.BackgroundJobs.Abstractions;
+using IReckonu.DataImportingTool.Data.Abstractions;
+using IReckonu.DataImportingTool.Data.Abstractions.File;
+using IReckonu.DataImportingTool.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace IReckonu.DataImportingTool.API
+{
+    [Route("api/[controller]")]
+    public class DataImportController : Controller
+    {
+        private readonly IDataImportApplicationService _dataImportApplicationService;
+
+        public DataImportController(IDataImportApplicationService dataImportApplicationService)
+        {
+            _dataImportApplicationService = dataImportApplicationService;
+        }
+       
+         [HttpPost("ImportDataFromFile")]
+        public async Task ImportDataFromFile(IFormFile file)
+        {
+            // Validation On the file
+            _dataImportApplicationService.ImportData(file.OpenReadStream());
+        }
+
+    }
+}
