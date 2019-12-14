d770c3c1d47b347fbd20d757ee494bf1e43e6e1e
Ayman Sharaf <ayman.sharaf@echotechit.com>
2019-12-14 22:42:21 +0300

    Add Error codes to controller

--- IReckonu.DataImportingTool/DataImportController.cs
+++ IReckonu.DataImportingTool/DataImportController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using IReckonu.DataImportingTool.Application.Abstractions;
 using IReckonu.DataImportingTool.BackgroundJobs.Abstractions;
@@ -25,10 +26,20 @@ namespace IReckonu.DataImportingTool.API
         }
        
          [HttpPost("ImportDataFromFile")]
-        public async Task ImportDataFromFile(IFormFile file)
+         [ProducesResponseType((int)HttpStatusCode.OK)]
+         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> ImportDataFromFile(IFormFile file)
         {
-            // Validation On the file
-            _dataImportApplicationService.ImportData(file.OpenReadStream());
+            if (file != null)
+            {
+                // Validation On the file
+                _dataImportApplicationService.ImportData(file.OpenReadStream());
+                return Ok();
+            }
+            else 
+            {
+                return BadRequest();
+            }
         }
 
     }
