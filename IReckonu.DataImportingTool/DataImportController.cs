2160f3a29c395f63fd6b088aea73c4d6096c24f8
Ayman Sharaf <ayman.sharaf@echotechit.com>
2019-12-21 12:53:45 +0300

    Remove Task from Controller

--- IReckonu.DataImportingTool/DataImportController.cs
+++ IReckonu.DataImportingTool/DataImportController.cs
@@ -5,10 +5,6 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using IReckonu.DataImportingTool.Application.Abstractions;
-using IReckonu.DataImportingTool.BackgroundJobs.Abstractions;
-using IReckonu.DataImportingTool.Data.Abstractions;
-using IReckonu.DataImportingTool.Data.Abstractions.File;
-using IReckonu.DataImportingTool.Domain.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -28,12 +24,12 @@ namespace IReckonu.DataImportingTool.API
          [HttpPost("ImportDataFromFile")]
          [ProducesResponseType((int)HttpStatusCode.OK)]
          [ProducesResponseType((int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> ImportDataFromFile(IFormFile file)
+        public IActionResult ImportDataFromFile(IFormFile file)
         {
             if (file != null)
             {
                 // Validation On the file
-                _dataImportApplicationService.ImportData(file.OpenReadStream());
+                 _dataImportApplicationService.ImportData(file.OpenReadStream());
                 return Ok();
             }
             else 
